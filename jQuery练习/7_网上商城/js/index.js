17006262611e2b6c06f5b9bf3cc4e897c7959e67
zhuhaixia99 <290813335@qq.com>
2016-11-06 21:44:33 +0800

    网上商城的js

--- jQuery练习/7_网上商城/js/index.js
+++ jQuery练习/7_网上商城/js/index.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2016/11/3.
  */
 $(function(){
+    /*input搜索框start*/
     var $input = $("#header .search input");
     $input.on("focus",function() {
         if (this.value == this.defaultValue) {
@@ -18,4 +19,48 @@ $(function(){
             alert(this.value);
         }
     });
+    /*input搜索框end*/
+    /*变换主题颜色start*/
+    var $skinLi = $("#skin li");
+    $skinLi.on("click",function(){
+        $("#"+this.id).addClass("selected").siblings().removeClass("selected");
+        $("#cssfile").attr("href","css/skin/"+ this.id +".css"); //设置不同皮肤
+    });
+    /*变换主题颜色end*/
+    /*轮播图start*/
+    var timer;
+    var $aImg = $("#img-turns a");
+    /*$aImg.each(function(index){
+        $(this).css({"z-index":$aImg.size()-index}) ;
+    });*/
+    $aImg.mouseenter(function(){
+        clearInterval(timer);
+    });
+    var $imgLi = $("#img-turns .img-link li");
+    var iNow = 0;
+    $imgLi.hover(function(){
+        changeImg($(this).index());
+        iNow = $(this).index();
+        clearInterval(timer);
+    },function(){
+        run();
+    });
+    function changeImg(index){
+        $imgLi.eq(index).addClass("selected").siblings().removeClass('selected');
+        $aImg.eq(index).stop(true).fadeIn().siblings().hide();
+    }
+    function run(){
+        timer = setInterval(function(){
+            iNow++;
+            if(iNow == $imgLi.size()){
+                iNow = 0;
+            }
+            changeImg(iNow);
+        },2000);
+    }
+    run();
+    /*轮播图end*/
+    /*下面的轮播图start*/
+
+    /*下面的轮播图end*/
 });
