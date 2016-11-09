a25294dd3f79bf9c74fd861fd54ab74c4b75cc5a
zhuhaixia99 <290813335@qq.com>
2016-11-09 22:29:57 +0800

    index的js部分优化了

--- jQuery练习/7_网上商城/js/index.js
+++ jQuery练习/7_网上商城/js/index.js
@@ -2,41 +2,18 @@
  * Created by Administrator on 2016/11/3.
  */
 $(function(){
-    /*input搜索框start*/
-    var $input = $("#header .search input");
-    $input.on("focus",function() {
-        if (this.value == this.defaultValue) {
-            this.value = "";
-        }
 
-    }).on('blur',function() {
-        if (this.value == '') {
-            this.value = this.defaultValue;
-        }
-    })
-    $input.on("keypress",function(event){
-        if ( event.which == 13 && this.value!="") {
-            alert(this.value);
-        }
-    });
-    /*input搜索框end*/
-    /*变换主题颜色start*/
-    var $skinLi = $("#skin li");
-    $skinLi.on("click",function(){
-        $("#"+this.id).addClass("selected").siblings().removeClass("selected");
-        $("#cssfile").attr("href","css/skin/"+ this.id +".css"); //设置不同皮肤
-    });
-    /*变换主题颜色end*/
-    /*轮播图start*/
+    /*杞挱鍥緎tart*/
     var timer;
-    var $aImg = $("#img-turns a");
+    var $imgTurns = $("#img-turns");
+    var $aImg = $("a",$imgTurns);
     /*$aImg.each(function(index){
         $(this).css({"z-index":$aImg.size()-index}) ;
     });*/
     $aImg.mouseenter(function(){
         clearInterval(timer);
     });
-    var $imgLi = $("#img-turns .img-link li");
+    var $imgLi = $(".img-link li",$imgTurns);
     var iNow = 0;
     $imgLi.hover(function(){
         changeImg($(this).index());
@@ -59,14 +36,15 @@ $(function(){
         },2000);
     }
     run();
-    /*轮播图end*/
-    /*下面的轮播图start*/
+    /*杞挱鍥緀nd*/
+    /*涓嬮潰鐨勮疆鎾浘start*/
     var $tabContent = $("#tab-content");
     var contentWidth = $tabContent.width();
     $tabContent.width(4 * contentWidth);
     $tabContent.html($tabContent.html() + $tabContent.html() + $tabContent.html() + $tabContent.html());
-    var $tabA = $("#tab a");
-    $("#tab li").mouseover(function(){
+    var $tab = $("#tab");
+    var $tabA = $("a",$tab);
+    $("li",$tab).mouseover(function(){
         $(this).addClass("selected").siblings().removeClass("selected");
         $tabA.removeClass("select");
         $tabA.eq($(this).index()).addClass("select");
@@ -74,6 +52,6 @@ $(function(){
             left:-$(this).index() * contentWidth
         },1000);
     });
-    /*下面的轮播图end*/
+    /*涓嬮潰鐨勮疆鎾浘end*/
 
 });
