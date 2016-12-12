731e1cf76bc56659fd38ab404fdf41b79e95658f
zhuhaixia99 <290813335@qq.com>
2016-12-12 21:00:08 +0800

    弹出层js

---
+++ requirejs/弹出层/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+require(["jquery","dialog"],function($,Dialog){
+    $('#open').on("click",function(){
+        var settings = {
+            width:500,
+            height:400,
+            title:"我的弹出层",
+            content:"login.html"
+        };
+        var dialog = new Dialog(settings);
+        dialog.open();
+    });
+});
